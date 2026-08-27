eb75e15a575e6cceeb3cae06e05556fe5d450ae8
HarelM <harel.mazor@gmail.com>
2026-08-27 10:59:48 +0300

    Add abort support

--- src/main/kotlin/io/ionic/libs/ionfiletransferlib/helpers/IONFLTRExceptionsMapper.kt
+++ src/main/kotlin/io/ionic/libs/ionfiletransferlib/helpers/IONFLTRExceptionsMapper.kt
@@ -5,12 +5,15 @@ import java.io.FileNotFoundException
 import java.io.IOException
 import java.net.ConnectException
 import java.net.SocketTimeoutException
+import kotlin.coroutines.cancellation.CancellationException
 
 internal inline fun <T> T.runCatchingIONFLTRExceptions(block: T.() -> Unit): Result<Unit> =
     runCatching(block).mapErrorToIONFLTRException()
 
 internal fun <R> Result<R>.mapErrorToIONFLTRException(): Result<R> =
     exceptionOrNull()?.let { throwable ->
+        // coroutine cancellation must never be swallowed, so that structured concurrency is kept
+        if (throwable is CancellationException) throw throwable
         val mappedException: IONFLTRException = when (throwable) {
             is IONFLTRException -> throwable
             is FileNotFoundException -> IONFLTRException.FileDoesNotExist(throwable)
